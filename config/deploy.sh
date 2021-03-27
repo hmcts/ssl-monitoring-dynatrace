e3ee37aa3fdef3978b805a8f2208047d1cf98107
Avinash Mutham <avinashm@Torreira.local>
2021-03-27 11:38:22 +0000

    monaco cli install and run

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-set -e
+MAC_VERSION="v1.5.0"
+MONACO_DOWNLOAD_URL="https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/$MAC_VERSION/monaco-linux-amd64"
+curl -SL $MONACO_DOWNLOAD_URL -o /usr/bin/monaco
+chmod +x /usr/bin/monaco
+
 monaco deploy --environments="dynatrace-environments.yaml" -p nonprod --continue-on-error=true
