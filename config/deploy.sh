228ad6434cd989295d5a7507b61cab9325133509
Avinash Mutham <avinashm@Torreira.local>
2021-03-27 12:04:02 +0000

    Fixing permission issues while installing monaco

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MAC_VERSION="v1.5.0"
 MONACO_DOWNLOAD_URL="https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/$MAC_VERSION/monaco-linux-amd64"
-curl -SL $MONACO_DOWNLOAD_URL -o /usr/bin/monaco
-chmod +x /usr/bin/monaco
+sudo curl -SL $MONACO_DOWNLOAD_URL -o /usr/bin/monaco
+sudo chmod +x /usr/bin/monaco
 
-monaco deploy --environments="dynatrace-environments.yaml" -p nonprod --continue-on-error=true
+sudo monaco deploy --environments="dynatrace-environments.yaml" -p nonprod --continue-on-error=true
