8d0f67b305f5651a1e380ba0f2e51d9725152a7b
Avinash Mutham <avinashm@Torreira.local>
2021-04-01 15:39:24 +0100

    disable exit on non 0

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set +e
 MAC_VERSION="v1.5.0"
 MONACO_DOWNLOAD_URL="https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/$MAC_VERSION/monaco-linux-amd64"
 sudo curl -SL $MONACO_DOWNLOAD_URL -o /usr/bin/monaco
