dbe706d1feefb052cbca7b318168dbfd1f4af054
Avinash Mutham <avinashm@Torreira.local>
2021-04-08 15:19:30 +0100

    clean up

--- apimgmt/go.mod
+++
@@ -1,3 +0,0 @@
-module apimgmt/m
-
-go 1.16
--- test.sh
+++
@@ -1,6 +0,0 @@
-function monaco_deploy(){
-    monaco deploy --environments="config/dynatrace-environments.yaml" -p "nonprod" --continue-on-error=true
-    return 0
-}
-monaco_deploy
-echo "Exit code $?"
